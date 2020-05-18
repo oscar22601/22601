3791cd0de7b9e79a29f167aace0ce6148d63c6e0
oscar22601 <65515326+oscar22601@users.noreply.github.com>
2020-05-18 12:04:43 +1200

    Add files via upload

---
+++ 2222.py
@@ -0,0 +1,9 @@
+number01 = int(input("enter your 01 number?"))
+number02 = int(input("enter your 02 number?"))
+answer = number01 + number02
+if answer % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+        
