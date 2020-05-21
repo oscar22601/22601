775e27f60ed90ac1806b021d791529e59a380efa
oscar22601 <65515326+oscar22601@users.noreply.github.com>
2020-05-22 06:54:30 +0800

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,10 @@
+print("that is healthy amount of time")
+internet_hours >=3
+internet_hours <=5
+print ("make sure you have some exercise as well")
+internet_hours >5
+internet_hours <=24
+else: 
+   print("There are only 24 hours in a day!")
+
+
